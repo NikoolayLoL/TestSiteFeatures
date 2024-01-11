28805bc5e936f377fc4a874532f2e3d0facc9e60
NikoolayLoL <123279411+NikoolayLoL@users.noreply.github.com>
2024-01-11 21:11:17 +0200

    Add project files.

---
+++ WebApp/Controllers/CategoriesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Category> categories = CategoriesRepository.GetCategories();
+            return View(categories);
+        }
+
+        [HttpGet]
+        public IActionResult Edit([FromRoute] int? id) // No longer accepts Querry Data ~/categories/edit/135
+        {                                              //[FromQuerry] Only from querry ~/categories/edit?id=135
+            //var category = new Category { CategoryID = (id.HasValue ? id.Value : 0) };
+
+            var category = CategoriesRepository.GetCategoryByID(id.HasValue ? id.Value : 0);
+
+            return View(category);
+
+
+            //return id.HasValue ? new ContentResult { Content = id.ToString() } 
+            //    : new ContentResult { Content = "Null Content" };
+
+            //Querry String edit?id=555 and URL edit/555 *Same thing!
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            CategoriesRepository.UpdateCategory(category.CategoryID, category);
+
+            //return View(category);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ WebApp/Models/CategoriesRepository.cs
@@ -0,0 +1,70 @@
+﻿namespace WebApp.Models
+{
+    public static class CategoriesRepository
+    {
+        private static List<Category> _categories = new List<Category>()
+        {
+            new Category()
+            {
+                CategoryID = 1, Name = "BMW", Description = "Car"
+            },
+            new Category()
+            {
+                CategoryID = 2, Name = "Mercedes", Description = "Car"
+            },
+            new Category()
+            { 
+                CategoryID = 3, Name = "Range Rover", Description = "Suv"
+            }
+        };
+
+        public static void AddCategory(Category category)
+        {
+            var maxId = _categories.Max(c => c.CategoryID);
+            category.CategoryID = maxId + 1;
+            _categories.Add(category);
+        }
+
+        public static List<Category> GetCategories() => _categories;
+
+        public static Category? GetCategoryByID(int categotyId)
+        {
+            var category = _categories.FirstOrDefault(x => x.CategoryID == categotyId);
+            if (category != null)
+            {
+                return new Category
+                {
+                    CategoryID = category.CategoryID,
+                    Name = category.Name,
+                    Description = category.Description
+                };
+            }
+
+            return null;
+        }
+
+        public static void UpdateCategory(int categoryId, Category category)
+        {
+            if (categoryId != category.CategoryID)
+            {
+                return;
+            }
+
+            var categoryToUpdate = GetCategoryByID(categoryId);
+            if (categoryToUpdate != null)
+            {
+                categoryToUpdate.Name= category.Name;
+                categoryToUpdate.Description= category.Description;
+            }
+        }
+
+        public static void DeleteCateforyById(int categoryId)
+        {
+            var category = _categories.FirstOrDefault(c => c.CategoryID == categoryId);
+            if (category != null)
+            {
+                _categories.Remove(category);
+            }
+        }
+    }
+}
